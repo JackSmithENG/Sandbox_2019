7fd979ffa9fe4a6b1f89daece3e94e5234366e65
JackSmithENG <jack.smith1@my.jcu.edu.au>
2019-08-20 12:35:16 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,16 @@
 """ Jack Smith """
+import random
+
+finished = False
+min_length = random.randint(5, 20)
+while not finished:
+    password = input("Please enter your {} character minimum password password: ".format(min_length))
+    if len(password) < min_length:
+        print("Password must be at least {} characters long".format(min_length))
+    else:
+        finished = True
+
+for i in password:
+    print("*", end=" ")
+
+print("\nYour password is valid")
